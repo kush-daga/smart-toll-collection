1e8adcca1583314556c104cb98e81043a22e0203
kush-daga <kushdaga@hotmail.com>
2019-02-10 22:58:25 +0530

    initial commit

--- app/views/main.py
+++ app/views/main.py
@@ -1,37 +1,19 @@
 from flask import Flask, request, render_template, flash, redirect, url_for, session, Blueprint
-from flask_mysqldb import MySQL
 from passlib.hash import sha256_crypt as sha
 from flask_session import Session;
 from app import *
-import requests
-import bs4
+from app.views.ml.Main import main
 
-main = Blueprint('main', __name__)
 
-@main.route('/test')
-def test():
-    res = requests.get('http://tweeplers.com/hashtags/?cc=IN')
-    soup = bs4.BeautifulSoup(res.text,'lxml')
-    new = soup.find_all( class_='wordwrap')
-    titles=[]
-    hashes=[]
-    urls=[]
+main2 = Blueprint('main2', __name__)
 
-    for tag in new:
-        res2 = requests.get('https://www.bing.com/news/search?q=india'+tag.text[1:]+'&FORM=HDRSC6')
-        soup2 = bs4.BeautifulSoup(res2.text, 'lxml')
-        new2 = soup2.find_all(class_="title")
-        for ans in new2:
-            titles.append(ans.text)
-            hashes.append(tag.text)
-            url =ans.get('href')
-            urls.append(url)
-    
-    all = zip(titles,hashes,urls)
-    
-
-    return render_template("test1.html", **locals())
-
-@main.route('/')
+@main2.route('/')
 def index():
+    a = main()
     return render_template("index.html", **locals())
+@main2.route('/form')
+def form():
+    return render_template("form.html", **locals())
+@main2.route('/generic')
+def generic():
+    return render_template("generic.html", **locals())    
--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from app import app
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
-    app.run(host = "0.0.0.0",debug=True, port=4000)
+    app.run(host = "127.0.0.1",debug=True, port=4000)
